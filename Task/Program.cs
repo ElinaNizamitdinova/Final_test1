e943ec051749ec709acf18a14709f6f870c988d0
Elina <elina.eldarovna.spb@gmail.com>
2023-12-05 16:40:23 +0300

    Print array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] startArray = {"Hello", "paradox", "new", "-9", "Sunshine", "1", "++++"};
+int rightLenght = 3;
+System.Console.WriteLine("-----");
+
+
+void PrintArray(string[] arr)
+{   System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if(i<arr.Length-1){
+            Console.Write(", ");
+        }
+        else{
+            Console.Write("");
+        }
+    }
+    System.Console.WriteLine("]");
+}
+
+// NewMatrix(startArray, rightLenght);
